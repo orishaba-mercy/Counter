dba154d3cd2888323c315e7cb6b1862a0e92289f
orishaba-mercy <orishabamercybanga@gmail.com>
2023-06-13 09:41:10 +0300

    m

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-let add = document.getElementById('increament');
-let deleting = document.getElementById('decreament')
- 
-let int = document.getElementById("number");
-let integer = 0;
+// Reference to incerement button
+const addBtn = document.getElementById("increament")
 
-add.addEventListener('click',function(){
-   integer +=1;
-   int.innerHTML=integer;
-});
-deleting.addEventListener('click',function(){
-    integer -=1;
-    int.innerHTML=integer;
+// Reference to decrement button
+const deleteBtn = document.getElementById("decreament")
+
+// Reference to restart button
+const restartBtn = document.getElementById("restart")
+
+// Reference to increment node
+const incrementNode = document.getElementById("number")
+
+// Reference to dynamic input
+const input = document.getElementById("incrementValue")
+
+// Increment reference
+let value = 0
+
+
+// Event Listener for Adding by 1
+
+addBtn.addEventListener("click", () => {
+
+    value= value + Number(input.value)
+
+    incrementNode.innerHTML = value
+
+
+
+})
+
+// Event Listener for removing by 1
+
+deleteBtn.addEventListener("click", () => {
+
+    value= value - Number(input.value)
+
+    incrementNode.innerHTML = value
+
+
+
+})
+
+// Event Listener for removing by 1
+
+restartBtn.addEventListener("click", () => {
+
+    value= 0
+
+    incrementNode.innerHTML = value
+
+
+
 })
